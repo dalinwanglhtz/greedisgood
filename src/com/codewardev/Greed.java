8624a134e5815a1bf0b3704653911c2b2e2b7fe4
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-15 23:26:22 +1100

    first cut

---
+++ src/com/codewardev/Greed.java
@@ -0,0 +1,35 @@
+package com.codewardev;
+
+// https://www.codewars.com/kata/greed-is-good/train/java
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Greed {
+
+	public static int greedy(int[] dice) {
+		int total=0;
+		Map<Integer, Integer> numbers = new HashMap<Integer, Integer>();
+		for(int x: dice) {
+			if(numbers.containsKey(x)) {
+				numbers.put(x, numbers.get(x)+1);
+			} else {
+				numbers.put(x, 1);
+			}
+		}
+		Iterator it = numbers.entrySet().iterator();
+		while(it.hasNext()) {
+			Map.Entry<Integer, Integer> elem = (Entry<Integer, Integer>) it.next();
+			
+		}
+		
+		int numOne = (int) Arrays.stream(dice).filter(x->x==1).count();
+
+		System.out.println("Num 1s: "+numOne);
+		return total;
+	}
+
+}
