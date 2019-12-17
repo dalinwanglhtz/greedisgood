06a5c739e4d44abc2c564ff22955b253ba9c06e3
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-17 23:07:54 +1100

    refine algorithm

--- src/com/codewardev/Greed.java
+++ src/com/codewardev/Greed.java
@@ -2,56 +2,22 @@ package com.codewardev;
 
 // https://www.codewars.com/kata/greed-is-good/train/java
 
-import java.util.Arrays;
-
 public class Greed {
 
 	public static int greedy(int[] dice) {
 		int total=0;
 
-		dice = Arrays.stream(dice).sorted().toArray();
-
-		int count=0;
-		for(int i=0; i<dice.length; i++) {
-			if(i!=0 && dice[i]==dice[i-1]) {
-				count++;
-			}
-			
-			if(i!=0 && dice[i]!=dice[i-1]){
-				count = 0;
-			}
-			
-			if(count == 2) {
-				if(dice[i] == 1) {
-					total -= 200;
-					total += 1000;
-				} else
-				if(dice[i] == 2) {
-					total += 200;
-				} else
-				if(dice[i] == 3) {
-					total += 300;
-				} else
-				if(dice[i] == 4) {
-					total += 400;
-				} else
-				if(dice[i] == 5) {
-					total -= 100;
-					total += 500;
-				} else
-				if(dice[i] == 6) {
-					total += 600;
-				}
-				count = i+1 != dice.length && dice[i] == dice[i+1] ? -1:0;
-			} else
-			if(count < 2 && dice[i] == 1) {
-				total += 100;
-			} else
-			if(count < 2 && dice[i] == 5) {
-				total += 50;
-			}
-			
+		int[] diceOccur = {0,0,0,0,0,0,0};
+		for(int j: dice) {
+			diceOccur[j]++;
 		}
+		
+		total += (diceOccur[1] > 2? 1000+(diceOccur[1]-3)*100: diceOccur[1]*100);
+		total += (diceOccur[2] > 2? 200: 0);
+		total += (diceOccur[3] > 2? 300: 0);
+		total += (diceOccur[4] > 2? 400: 0);
+		total += (diceOccur[5] > 2? 500+(diceOccur[5]-3)*50: diceOccur[5]*50);
+		total += (diceOccur[6] > 2? 600: 0);
 
 		return total;
 	}
