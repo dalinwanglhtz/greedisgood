601779b78a596eea50b3ba0fa2f7f16ce200e524
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-18 20:47:32 +1100

    turn into loop

--- src/com/codewardev/Greed.java
+++ src/com/codewardev/Greed.java
@@ -8,16 +8,18 @@ public class Greed {
 		int total=0;
 
 		int[] diceOccur = {0,0,0,0,0,0,0};
-		for(int j: dice) {
-			diceOccur[j]++;
-		}
+		for(int j: dice) diceOccur[j]++;
 		
-		total += (diceOccur[1] > 2? 1000+(diceOccur[1]-3)*100: diceOccur[1]*100);
-		total += (diceOccur[2] > 2? 200: 0);
-		total += (diceOccur[3] > 2? 300: 0);
-		total += (diceOccur[4] > 2? 400: 0);
-		total += (diceOccur[5] > 2? 500+(diceOccur[5]-3)*50: diceOccur[5]*50);
-		total += (diceOccur[6] > 2? 600: 0);
+		for(int i=1; i<diceOccur.length; i++) {
+			if(i==1) {
+				total += (diceOccur[i] > 2? 1000+(diceOccur[1]-3)*100: diceOccur[1]*100);
+			} else
+			if(i==5) {
+				total += (diceOccur[i] > 2? 500+(diceOccur[5]-3)*50: diceOccur[5]*50);
+			} else {
+				total += (diceOccur[i] > 2? i*100: 0);
+			}
+		}
 
 		return total;
 	}
